acdf53ff2ff902f93241aade719266def69920e5
rubenvisser22 <rubenvisser1@hotmail.com>
2021-08-24 20:20:58 +0200

    how to buy verder aan gewerkt

--- src/containers/HowToBuy.js
+++ src/containers/HowToBuy.js
@@ -6,43 +6,86 @@ import media from '../styles/media';
 
 const HowToBuyWrapper = css`
     font-family: 'Nunito', monospace;
-    padding-left: 2%;
-    padding-right: 2%;
+    padding: 2%;
     text-align: center;
-    padding-bottom: 2%;
     background-color: rgba(13, 13, 44);
+    line-height: 2rem;
 
     h1{
+        font-size: 4rem;
+        font-weight: 500;
         text-align: center;
-        color: white;
         background-color: rgb(23, 23, 73);
         padding: 2%;
-        margin-bottom: 0px!important;
-        font-size: 2.5rem;
+    }
+
+    h4{
+        font-size: 24px;
+        padding-bottom: 2%;
+        line-height: 3rem;
+        text-align: left;
     }
     p{
-        text-align: center;
+        text-align: left;
         font-size: 16px;
-        line-height: 2rem;
         font-weight: 600;
         letter-spacing: .0625em;
+    }
+    a{
+        cursor: pointer;
+        color: #FDD700;
+        border-bottom: 1px solid #FDD700;
     }
 `;
 
 const TextContainer = css`
-    text-align: center;
-    color: white;
-    background-color: rgba(13, 13, 44);
-    padding: 2%;
-    margin-bottom: 0px!important;
+text-align: center;
+color: white;
+background-color: rgba(13, 13, 44);
+margin-bottom: 0px!important;
+`;
+const containerGrid = css`
+display: inline-grid;
+grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+width: 100%;
 `;
 
+const gridItem = css`
+padding: 3%;
+width: 100%;
+height: auto;
+background-color: rgb(23, 23, 73);
+`;
 
 const HowToBuy = () => {
     return (
         <section css={HowToBuyWrapper}>
             <div css={TextContainer}>
                 <h1 id="HowToBuy">How To Buy</h1>
+            </div>
+            <div css={containerGrid}>
+                <div css={gridItem}>
+                    <h4>Download for example the app: "Trustwallet"</h4>
+                    <p>Write down your recovery phrase. Store this phrase in a safe place and never share this phrase with anyone.</p>
+                </div>
+                <div css={gridItem}>
+                    <h4>Buy BNB Smartchain token</h4>
+                    <p>Go to the BNB Smartchain token inside the app. Click on "Buy" at the right top and select the amount of BNB Smart chain tokens you want to buy.
+                        If this is not available in your app due to your country, buy BNB on an exchange and send this to your Trustwallet.</p>
+                </div>
+                <div css={gridItem}>
+                    <h4>Enable "browser" inside the app (only needed when having an iPhone)</h4>
+                    <p>Open the trustwallet app. Go to the browser tap and search for pancakeswap.<b>When having an android: </b>Just click Dapps or Browser button on the bottom inside the trustwallet app..</p>
+                </div>
+                <div css={gridItem}>
+                    <h4>Search for Pancakeswap</h4>
+                    <p>Open the trustwallet app. Go to the browser tap and search for pancakeswap.</p>
+                </div>
+                <div css={gridItem}>
+                    <h4>Add Algorithm Coin (ALGRTHM) to both Trustwallet and Pancakeswap</h4>
+                    <p>You will have to add your token both to Trustwallet and Pancakeswap inside Trustwallet. Here is more information about how to add a custom token:
+                        <a href="https://community.trustwallet.com/t/how-to-add-a-custom-token/213">https://community.trustwallet.com/t/how-to-add-a-custom-token/213</a> </p>
+                </div>
             </div>
         </section>
     );
