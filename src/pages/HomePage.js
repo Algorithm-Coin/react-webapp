28ac1a3a3b348bd76c0129fccf1c109155ec6988
Lars van der Niet <larsvanderniet@gmail.com>
2021-08-26 11:39:51 +0200

    Made Intro responsive

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { css } from '@emotion/react';
+
 import { HowToBuy, Intro, Mission, Roadmap } from '../containers';
+import media from '../styles/media';
 
 const HomePageStyle = css`
     h1 {
@@ -10,6 +12,24 @@ const HomePageStyle = css`
     }
     h3 {
         font-size: 3em;
+    }
+
+    ${media.small} {
+        h1 {
+            font-size: 4rem;
+        }
+        h3 {
+            font-size: 2em;
+        }
+    }
+
+    ${media.xsmall} {
+        h1 {
+            font-size: 3rem;
+        }
+        h3 {
+            font-size: 1.66em;
+        }
     }
 `;
 
