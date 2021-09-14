1d050813b62f48b0ec5d1bfe84f85dc2ca4c2d1c
rubenvisser22 <rubenvisser1@hotmail.com>
2021-09-14 10:03:41 +0200

    counterhelemaalweg

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,7 @@ const HomePage = () => {
     return (
         <>
             <Intro />
-            <Count />
+            {/* <Count /> */}
             <Mission />
             <HowToBuy />
             <Roadmap />
